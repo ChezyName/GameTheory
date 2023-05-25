e193ebbdfbcecfb93f94742f9856d8e4cd65a285
ChezyName <mineboy2468@gmail.com>
2023-05-25 18:29:03 -0500

    Centered Both Images

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,53 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import AllFour from "./assets/TheoryRing.png";
+import Trophy from "./assets/GameTheoryTrophy.png";
 
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{
+        height: "100vh",
+        width: '100vw',
+        display: 'flex',
+        justifyContent: "center",
+        alignItems: "center",
+        position: "absolute"
+      }}>
+        <img
+          id="OuterImage"
+          src={AllFour} alt="Four Rings"
+          height="550vw"
+          width="550vw"
+          style={{
+            userSelect: 'none',
+          }}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div style={{
+        height: "100vh",
+        width: '100vw',
+        display: 'flex',
+        justifyContent: "center",
+        alignItems: "center",
+        position: "absolute"
+      }}>
+        <img
+          id="InnerImage"
+          src={Trophy} alt="Trophy"
+          height="550vw"
+          width="550vw"
+          style={{
+            userSelect: 'none',
+          }}
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      <div id="GAMETHEORY">
+
+      </div>
     </>
   )
 }
