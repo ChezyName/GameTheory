2ad25f853e27c73cc912cd132f51da0713017014
ChezyName <mineboy2468@gmail.com>
2023-05-25 21:36:40 -0500

    Added 4 Quadrants

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ function App() {
           width="550vw"
           style={{
             userSelect: 'none',
+            zIndex: "5"
           }}
         />
       </div>
@@ -41,13 +42,50 @@ function App() {
           width="550vw"
           style={{
             userSelect: 'none',
+            zIndex: "5"
           }}
         />
       </div>
       
-      <div id="GAMETHEORY">
+      <div id="GAME" style={{
+        width: "50vw",
+        height: "50vh",
+        position: "absolute",
+        zIndex: '0',
+        left: "0",
+        top: "0",
+        backgroundColor: "rgba(1,227,31,0.25)"
+      }}></div>
 
-      </div>
+      <div id="FILM" style={{
+        width: "50vw",
+        height: "50vh",
+        position: "absolute",
+        zIndex: '0',
+        left: "50vw",
+        top: "0",
+        backgroundColor: "rgba(254, 0, 2,0.25)"
+      }}></div>
+
+      <div id="FOOD" style={{
+        width: "50vw",
+        height: "50vh",
+        position: "absolute",
+        zIndex: '0',
+        left: "0vw",
+        top: "50vh",
+        backgroundColor: "rgba(255,255,1,0.25)"
+      }}></div>
+
+      <div id="STYLE" style={{
+        width: "50vw",
+        height: "50vh",
+        position: "absolute",
+        zIndex: '0',
+        left: "50vw",
+        top: "50vh",
+        backgroundColor: "rgba(0,192,255,0.25)"
+      }}></div>
     </>
   )
 }
